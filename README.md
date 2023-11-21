bfbc788c5c71c605769ec831f91b9a64b7ed6181
Martin AGBO <66011023+agboba@users.noreply.github.com>
2023-11-21 18:48:43 +0000

    Premier commit : Ajout des fichiers initiaux

--- README.md
+++ README.md
@@ -1,1 +1,63 @@
-# Togo-Tech-Communities
+# RdcTechCommunities
+
+Ce projet a pour but de mettre à la dispositions des tout le monde, des informations sur les communauté Technologiques du Togo, des infos commes leur site web, les differents liens de leurs comptes sur les reseaux sociaux ainsi que leurs github pour certains.
+
+## la Stack du projet
+
+- Flask (Micro Framework de Python)
+- Tailwindcss ( tailwindcss.com )
+- Boxicons (  boxicons.com)
+- Python 3
+- Sqlite3
+
+Si votre communauté n'est pas dans le repo vous avez la possibilité de l'ajouter directement sur le site web vous même via ce lien  `fork`  ou
+ via un PR ou faite le moi savoir pour que je l'ajoute.
+
+## Ajout de la communauté par PR
+
+- Les Etapes :
+
+  - Forker ce repo en appuyant sur le bouton `fork`
+  - Cloner le repo sur votre machine avec `git clone https://github.com/Likeur/RdcTechCommunities.git`
+
+  - Creer une nouvelle Branche avec `git Branch nom_de_la_branche`
+  - Switcher vers la nouvelle branche avec `git checkout votre_branche`
+  - Naviguer vers le dossier cloné avec `cd RdcTechCommunities`
+  - L'ouvrir avec vs code ou votre éditeur de text preféré
+  - Naviguer vers le dossier `static/assets/communities`
+
+  - puis ouvrez le fichier `data.json`
+
+    Vous y trouverez une Liste d'objet
+
+  - dans cette liste d'objet ajoutez un nouvel objet comme  ceci :
+
+    `
+     , {
+    id : +1,
+        community : {
+                name: 'nom_de_la_communauté',
+                linkedin: 'lien',
+                whatsapp : 'lien',
+                youtube : 'lien',
+                github : 'lien',
+             siteweb: 'lien'
+            }
+        },
+    `
+    (Si vous ne mettez pas de lien  laisser l'endroit vide comme ceci `''`)
+
+    " sur l'id iterez en ajoutant un +1 sur le nombre précedant et ça sera bon " ,
+
+  - Enregistrez le fichier avec `ctrl+s`
+  - Sauvegardez vos modifications avec `git add .`
+  - Faites un commit avec un message de vos modifications avec `git commit -m "votre_message"`
+  - Faites un push vers le repos forker dans votre nouvelle branche avec `git push -u remote_name branch_name`
+
+  - puis rentrez sur  github pour faire un `PR` (pull request) ensuite je vais `merge`  les modifs après verification.
+
+    Et voilà normalement si vous avez suivis toutes les  etapes votre communauté sera sur la liste.
+
+proudly made by ``AGBO Martin``
+
+NB: c'est un OpenSource et ouvert à la contrubition inspirer par le project de RdcTechCommunities `https://github.com/Likeur/RdcTechCommunities.git` made by   ``Likeur off``
---
+++ app.py
@@ -0,0 +1,80 @@
+from flask import Flask , render_template,request,url_for,redirect
+import random , os
+from difflib import get_close_matches as commpare
+import sqlite3
+
+
+app = Flask(__name__)
+app.secret_key = 'jkxnkvbx!m knnkdbvl:xl, XBBs'
+upload_folder = "static/assets/img/"
+allowed_exta= ["jpg","png","jpeg","svg"]
+app.config["UPLOAD_FOLDER"] = upload_folder
+
+conn = sqlite3.connect("static/assets/database.db",check_same_thread=False)
+conn.row_factory = sqlite3.Row
+cursor = conn.cursor()
+
+def allowed_file(image):
+    extansion = image.filename.split(".")[-1].lower()
+    if "." in image.filename and extansion in allowed_exta:
+        filename = (f'aa_{random.randint(1,1000)}{random.randint(1000,10000)}_community_image.{extansion}')
+        image.save(os.path.join(app.config["UPLOAD_FOLDER"],filename))
+        return os.path.join(app.config["UPLOAD_FOLDER"],filename)
+    else:
+        return os.path.join(app.config["UPLOAD_FOLDER"],"1.jpg")
+
+
+@app.route('/')
+def home():
+    result= cursor.execute("select * from users")
+    datas= [dict(d) for d in result.fetchall()]
+    return render_template("index.html",datas = datas ,isEmpty = len(datas))
+
+@app.route('/add' , methods = ["POST"])
+def add():
+    if request.method == "POST":
+        name = request.form.get("name")
+        tag = request.form.get("tag")
+        about = request.form.get("about")
+        if "image" in request.files:
+            image = request.files.get("image")
+            filename = allowed_file(image)
+        else:
+            filename = os.path.join(app.config["UPLOAD_FOLDER"],"1.jpg")
+        linkedin = request.form.get("linkedin")
+        website = request.form.get("website")
+        github = request.form.get("github")
+        youtube = request.form.get("youtube")
+        whatsapp = request.form.get("whatsapp")
+        x = request.form.get("x")
+        facebook = request.form.get("facebook")
+        telegram = request.form.get("telegram")
+
+        datas = (filename,name,about,linkedin,whatsapp,youtube,github,website,facebook,x,telegram,tag)
+        req = '''INSERT INTO  "users" ("image", "name", "about", "linkedin", "whatsapp", "youtube", "github", "web", "facebook", "x", "telegram","tag" ) 
+        VALUES (?, ?, ?, ?, ?, ?, ? ,?, ?, ?, ?,?)'''
+
+        cursor.execute(req,datas)
+        conn.commit()
+        return redirect(url_for('addone'))
+
+@app.route('/search' , methods = ["POST"])
+def search():
+    qery = request.form.get("search")
+    if qery == '':
+        result= cursor.execute("select * from users")
+        datas= [dict(d) for d in result.fetchall()]
+        return render_template('index.html',datas = datas ,isEmpty = len(datas))
+    result = cursor.execute("SELECT * FROM users WHERE name LIKE ? OR about LIKE ?",('%'+qery+'%' , '%'+qery+'%'))
+    datas = [dict(data) for data in result.fetchall()]
+    return render_template('index.html',datas = datas,isEmpty = len(datas))
+
+
+@app.route('/addone')
+def addone():
+    result= cursor.execute("select * from users")
+    datas= [dict(d) for d in result.fetchall()]
+    return render_template("compo.html",datas = datas)
+
+if "__main__" == __name__:
+    app.run(debug=True, port=8808)
